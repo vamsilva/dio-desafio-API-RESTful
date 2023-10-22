57331904be0958bd84b8c7c8fb14c766c051250c
Victor Marisau <victor.marisau@gmail.com>
2023-10-22 20:51:26 -0300

    Entrega do projeto

---
+++ src/main/java/me/dio/Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderBootcampApplication.class, args);
+    }
+
+}
